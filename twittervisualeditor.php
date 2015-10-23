68f9a613086c2830d3f23e0c79baa9df6fc4aecf
Pedro Pinto <pedropintomena@gmail.com>
2015-10-23 12:50:49 +0200

    initial repo

---
+++ twittervisualeditor.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Tittervisualeditorlogin
+Version: 1.0
+Description: Add button 'Share on Twitter' on Visual Editor
+Author: Pinto
+Author URI: http://bitado.com
+Plugin URI: http://bitado.com
+Text Domain: twittervisualeditor
+Domain Path: /languages
+*/
